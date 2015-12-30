9248841d64f67430558d31b04f6c610417960391
Rakesh.P.D <errakeshpd@gmail.com>
2015-12-30 09:44:51 +0530

    pace configuarion included

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,11 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+window.paceOptions = {
+  // Disable the 'elements' source
+  elements: false,
+  // Only show the progress on regular and ajax-y page navigation,
+  // not every request
+  restartOnRequestAfter: false
+}
